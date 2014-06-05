b2ab5eec97fe31ebd4f18bd9defdc336b767c0f7
lijenniferx <lijenniferx@gmail.com>
2014-06-05 11:50:44 -0400

    Edited get_gapes
    get_gapes now outputs a cell array labeling individual movements as
    gape or lick.

--- get_gapes.m
+++ get_gapes.m
@@ -11,14 +11,19 @@
 %                    'onset'- 1 is placed at the onset of the gape
 %                    'peak'-1 is placed at the peak of the gape
 % OUTPUTS
-% __gape_array:       A cell array of equal size as emg. Each element of the
-%                     cell array is a matrix with dimensions trial x time
+% __gape_array:       A cell array of equal size as emg. 
+%                     Each element of the cell array is a matrix with dimensions trial x time
+% __movement_id:      A cell array of equal size as emg. Each element of
+%                     cell array is a cell array that represents the trial
+%                     number: movement_id{taste}{trial_number}. Each trial
+%                     is a vector whose length is the number of movements.
+%                     0's denote licks, and 1's gapes. 
 % HELPER FUNCTIONS
 % __burst_features(): Calculates the features for individual movements
 
 %Written by Jennifer Li
 
-function gape_array=get_gapes(emg,onset_or_peak)
+function [gape_array,movement_id]=get_gapes(emg,onset_or_peak)
 
 if nargin==1
     onset_or_peak='peak';
@@ -45,7 +50,7 @@ end
     current_path=pwd;
 
     cd /Users/jenniferli/Documents/MatlabFunctions/KatzLab/EMG/
-    load QDA_nostd_no_first
+     load QDA_nostd_no_first
     coefs=important_coefficients;
     
     
@@ -63,12 +68,17 @@ end
 
     trial_length=size(emg{1},2);
     
+    
+    
     for tastes=1:length(features) %% loop through each taste
         
         no_of_trials=size(features{tastes},2);
-        gape_array{tastes}=zeros(no_of_trials,trial_length); %%% preallocating 
+        gape_array{tastes}=zeros(no_of_trials,trial_length); %%% preallocating gape_array
+        
         
         for trial=1:no_of_trials %% loop through each trial
+            
+            movement_id{tastes}{trial}=zeros(1,size(features{tastes}{trial},1)); %%% preallocating movement_id, all movements are initially licks by default and have value 0
             
             peak_time=features{tastes}{trial}(:,1);
             onset_time=features{tastes}{trial}(:,1)+features{tastes}{trial}(:,5);
@@ -79,13 +89,15 @@ end
             condition_2=ones(size(condition_1,1),size(condition_1,2)); condition_2(1)=0;  %% can't be the first movement
             
             
-            conditions_all=condition_1&condition_2; %% both conditions must be met
+             conditions_all=condition_1&condition_2; %% both conditions must be met
             
             if strcmp(onset_or_peak,'peak')
                 gape_array{tastes}(trial,peak_time(conditions_all))=1;  %%%% use peak  as time of gape
             else
                 gape_array{tastes}(trial,onset_time(conditions_all))=1;  %%% use onset as time of gape
             end
+            
+            movement_id{tastes}{trial}(conditions_all)=1;  %% set movements corresponding to gapes to 1
 
             
         end
